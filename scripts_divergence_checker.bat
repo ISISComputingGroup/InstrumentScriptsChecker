4b3a55e6c20e47b71e96be7d1e261de12145d662
Isaac Hill <isaac.hill@stfc.ac.uk>
2023-12-07 16:38:23 +0000

    tidying up again

--- scripts_divergence_checker.bat
+++ scripts_divergence_checker.bat
@@ -1,7 +1,5 @@
 setlocal
 
-REM Create local python environment from genie python on share
-REM git clone https://github.com/ISISComputingGroup/ibex_utils.git
 CALL \\isis\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat
 
 %LATEST_PYTHON% -u scripts_divergence_checker.py
