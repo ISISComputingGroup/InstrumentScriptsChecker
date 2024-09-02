144ba7314ee7b1d555f7a33ac2e38cbbd4f09215
Jack Doughty <jackb.doughty@btinternet.com>
2024-09-02 11:23:35 +0100

    Update scripts_divergence_checker to install/remove python

--- scripts_divergence_checker.bat
+++ scripts_divergence_checker.bat
@@ -3,4 +3,11 @@ setlocal
 CALL \\isis\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat
 
 %LATEST_PYTHON% -u scripts_divergence_checker.py
-if %errorlevel% neq 0 exit /b %errorlevel%
+
+if %errorlevel% neq 0 (
+    set errcode = %ERRORLEVEL%
+    call \\isis\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\remove_genie_python.bat %LATEST_PYTHON_DIR%
+    EXIT /b !errcode!
+)
+
+call \\isis\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\remove_genie_python.bat %LATEST_PYTHON_DIR%
